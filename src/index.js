d6320cc80732236b421f9512f81f7efa506d26dd
GytisJuozenas <gytis.juozenas@gmail.com>
2019-10-24 18:27:52 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import './assets/style.scss'
+import './assets/img/logo@1X.png';
+import './assets/img/Crayon@1X.png';
+import './assets/img/Flag@1X.png';
+import './assets/img/Rocket@1X.png';
+import './assets/img/Shape@1X.png';
+import './assets/img/LinkedIn@1X.png';
+import './assets/img/Mail@1X.png';
+import './assets/img/Facebook@1X.png';
+import './assets/img/Twitter@1X.png';
+import './assets/img/comp1@1X.png';
+import './assets/img/comp2@1X.png';
+import './assets/img/comp3@1X.png';
+import './assets/img/comp4@1X.png';
+
+// import Icon from './assets/img/unnamed.png';
+
+// const img = new Image()
+// img.src = Icon
+
+
+// const div = document.createElement('div')
+// document.querySelector('body').appendChild(div);
+// document.querySelector('div').appendChild(img);
